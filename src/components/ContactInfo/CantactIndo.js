8dea2107dc795ae38f6850150034469321629d70
Aditya <adijr9487@gmail.com>
2021-03-28 17:03:03 +0530

    Second Commit
    Before ejecting

---
+++ src/components/ContactInfo/CantactIndo.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+//components
+
+//classes
+
+const CantactIndo = (peops) => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default CantactIndo
---
+++ src/components/Utility/Auxiliary.js
@@ -0,0 +1,2 @@
+import React from "react";
+export const Auxiliary = (props) => <div>{props.children}</div>;
